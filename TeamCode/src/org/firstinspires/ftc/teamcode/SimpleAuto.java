6560fe616b1b6f8752b2225146eeaeb8edb38861
Stephan Roorda <roorda@us.ibm.com>
2020-10-28 21:28:08 -0400

    add SimpleAuto

---
+++ TeamCode/src/org/firstinspires/ftc/teamcode/SimpleAuto.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "SimpleAuto", group = "TwoWheelBot")
+public class SimpleAuto extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        DcMotor leftMotor = hardwareMap.dcMotor.get("left_motor");
+
+        telemetry.addData("Status","Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        leftMotor.setPower(0.25f);
+        while (opModeIsActive() && !isStopRequested()) {
+            telemetry.addData("Status","Running");
+            telemetry.update();
+        }
+
+        leftMotor.setPower(0);
+    }
+}
