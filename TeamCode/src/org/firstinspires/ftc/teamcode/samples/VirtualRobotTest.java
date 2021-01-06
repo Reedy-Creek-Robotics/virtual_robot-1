07161fc44e668be203f44cfe7496b6e6a8ef72ad
VProPlayer <vkchaudhari2007@icloud.com>
2021-01-06 18:11:43 -0500

    Commit X-Wing 2.1

--- TeamCode/src/org/firstinspires/ftc/teamcode/samples/VirtualRobotTest.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/samples/VirtualRobotTest.java
@@ -4,10 +4,11 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
-@TeleOp(name = "VedantTeleOpCode", group = "")
-public class MotorThingByVedant extends LinearOpMode {
+@TeleOp(name = "RobotTest", group = "")
+public class VirtualRobotTest extends LinearOpMode {
 
     private static int BUTTON_DELAY = 750;
 
@@ -15,7 +16,11 @@ public class MotorThingByVedant extends LinearOpMode {
     public DcMotor backLeft;
     public DcMotor frontRight;
     public DcMotor backRight;
-
+    /*public Servo grabber;
+    public DcMotor intake;
+    public DcMotor shooter;
+    public DcMotor lifter;
+    public Servo loader;*/
 
     /**
      * This function is executed when this Op Mode is selected from the Driver Station.
@@ -26,6 +31,12 @@ public class MotorThingByVedant extends LinearOpMode {
         backLeft = hardwareMap.dcMotor.get("backLeft");
         frontRight = hardwareMap.dcMotor.get("frontRight");
         backRight = hardwareMap.dcMotor.get("backRight");
+        /*grabber = hardwareMap.servo.get("grabber");
+        intake = hardwareMap.dcMotor.get("intake");
+        shooter = hardwareMap.dcMotor.get("shooter");
+        lifter = hardwareMap.dcMotor.get("lifter");
+        loader = hardwareMap.servo.get("loader");*/
+
 
         boolean isBPressed = false;
         boolean isAPressed = false;
@@ -45,7 +56,7 @@ public class MotorThingByVedant extends LinearOpMode {
 
 
                 if (gamepad1.a && timeSinceLastPress.milliseconds() >= BUTTON_DELAY) {
-                    // Turn the motor at 0.25 power when a button is pressed, and turn it off once a is pressed again.
+                    // Press A to turn it on and off.
                     if (frontLeft.getPower() > 0) {
                         frontLeft.setPower(0);
                     } else {
@@ -58,7 +69,7 @@ public class MotorThingByVedant extends LinearOpMode {
                 }
 
                 if (gamepad1.b && timeSinceLastPress.milliseconds() >= BUTTON_DELAY) {
-                    // Turn the motor at 0.25 power when a button is pressed, and turn it off once a is pressed again.
+                    // Press B to turn it on and off.
                     if (frontRight.getPower() > 0) {
                         frontRight.setPower(0);
                     } else {
@@ -70,7 +81,7 @@ public class MotorThingByVedant extends LinearOpMode {
                     timeSinceLastPress.reset();
                 }
                 if (gamepad1.x && timeSinceLastPress.milliseconds() >= BUTTON_DELAY) {
-                    // Turn the motor at 0.25 power when a button is pressed, and turn it off once a is pressed again.
+                    // Press X to turn it on and off.
                     if (backLeft.getPower() > 0) {
                         backLeft.setPower(0);
                     } else {
@@ -81,7 +92,7 @@ public class MotorThingByVedant extends LinearOpMode {
                     timeSinceLastPress.reset();
                 }
                 if (gamepad1.y && timeSinceLastPress.milliseconds() >= BUTTON_DELAY) {
-                    // Turn the motor at 0.25 power when a button is pressed, and turn it off once a is pressed again.
+                    // Press Y to turn it on and off.
                     if (backRight.getPower() > 0) {
                         backRight.setPower(0);
                     } else {
@@ -93,13 +104,75 @@ public class MotorThingByVedant extends LinearOpMode {
                     timeSinceLastPress.reset();
 
                 }
+                /*if (gamepad1.dpad_down && timeSinceLastPress.milliseconds() >= BUTTON_DELAY) {
+                    // Press DPad Downto turn it on and off.
+                    if (shooter.getPower() > 0) {
+                        shooter.setPower(0);
+                    } else {
+                        shooter.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                        shooter.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                        shooter.setPower(0.25);
+                    }
+
+                    timeSinceLastPress.reset();
+
+                }
+                if (gamepad1.dpad_up && timeSinceLastPress.milliseconds() >= BUTTON_DELAY) {
+                    // Press DPad Up to turn it on and off.
+                    if (intake.getPower() > 0) {
+                        intake.setPower(0);
+                    } else {
+                        intake.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                        intake.setPower(0.25);
+                    }
+
+                    timeSinceLastPress.reset();
+
+                }
+                if (gamepad1.dpad_left && timeSinceLastPress.milliseconds() >= BUTTON_DELAY) {
+                    // Press DPad Left to turn it on and off.
+                    if (grabber.getPosition() > 0) {
+                        grabber.setPosition(0);
+                    } else {
+                        grabber.setPosition(0.25);
+                    }
+
+                    timeSinceLastPress.reset();
+
+                }
+                if (gamepad1.dpad_right && timeSinceLastPress.milliseconds() >= BUTTON_DELAY) {
+                    // Press DPad Right to turn it on and off.
+                    if (lifter.getPower() > 0) {
+                        lifter.setPower(0);
+                    } else {
+                        lifter.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                        lifter.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                        lifter.setPower(0.25);
 
 
+                    }
+                    timeSinceLastPress.reset();
+                }
+
+                if (gamepad1.left_trigger && timeSinceLastPress.milliseconds() >= BUTTON_DELAY) {
+                    // Press Left Trigger to turn it on and off.
+                    if (loader.getPosition() > 0) {
+                       loader.setPosition(0);
+                    } else {
+                        loader.setPosition(0.25);
+                    }
+                }*/
                 telemetry.addData("change number", 1.1);
                 telemetry.addData("A - Front Left", frontLeft.getPower()+","+frontLeft.getCurrentPosition());
                 telemetry.addData("B - Front Right", frontRight.getPower()+","+frontRight.getCurrentPosition());
                 telemetry.addData("X - Back Left", backLeft.getPower()+","+backLeft.getCurrentPosition());
                 telemetry.addData("Y - Back Right", backRight.getPower()+","+backRight.getCurrentPosition());
+                /*telemetry.addData("DPad Down - Shooter", shooter.getPower()+",");
+                telemetry.addData("DPad Up - Intake", intake.getPower()+",");
+                telemetry.addData("DPad Left - Grabber", grabber.getPosition()+",");
+                telemetry.addData("DPad Right - Lifter", lifter.getPower()+",");
+                telemetry.addData("Left Trigger - Loader", loader.getPosition()+",");*/
                 telemetry.update();
             }
         }
