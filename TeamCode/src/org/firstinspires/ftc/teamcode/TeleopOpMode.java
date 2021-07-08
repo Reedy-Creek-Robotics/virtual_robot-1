b940aa768d40f5b637a00d3e07952d6672421650
minerkid08 <85370763+minerkid08@users.noreply.github.com>
2021-07-08 16:41:45 -0400

    Add files via upload

--- TeamCode/src/org/firstinspires/ftc/teamcode/TeleopOpMode.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/TeleopOpMode.java
@@ -2,44 +2,42 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.text.DecimalFormat;
 @TeleOp(name = "test opmode")
 public class TeleopOpMode extends LinearOpMode {
-    DcMotor m1, m2, m3, m4;
-    public void init(){
-        m1 = hardwareMap.dcMotor.get("back_left_motor");
-        m2 = hardwareMap.dcMotor.get("front_left_motor");
-        m3 = hardwareMap.dcMotor.get("front_right_motor");
-        m4 = hardwareMap.dcMotor.get("back_right_motor");
-        m1.setDirection(DcMotor.Direction.REVERSE);
-        m2.setDirection(DcMotor.Direction.REVERSE);
-        m1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        m2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        m3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        m4.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    }
+    private DcMotor frontLeft;
+    private DcMotor backLeft;
+    private DcMotor frontRight;
+    private DcMotor backRight;
     public void runOpMode() {
-        float forward = gamepad1.left_stick_y * -1;
-        float strafe = gamepad1.left_stick_x;
-        float rotate = gamepad1.right_stick_x;
-        setPower(forward, strafe, rotate);
-    }
-    void setPower(float px, float py, float pa){
-        double p1 = -px + py - pa;
-        double p2 = px + py + -pa;
-        double p3 = -px + py + pa;
-        double p4 = px + py + pa;
-        double max = Math.max(1.0, Math.abs(p1));
-        max = Math.max(max, Math.abs(p2));
-        max = Math.max(max, Math.abs(p3));
-        max = Math.max(max, Math.abs(p4));
-        p1 /= max;
-        p2 /= max;
-        p3 /= max;
-        p4 /= max;
-        m1.setPower(p1);
-        m2.setPower(p2);
-        m3.setPower(p3);
-        m4.setPower(p4);
+        /*frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        backRight = hardwareMap.dcMotor.get("backRight");*/
+        frontLeft = hardwareMap.dcMotor.get("back_left_motor");
+        backLeft = hardwareMap.dcMotor.get("front_left_motor");
+        frontRight = hardwareMap.dcMotor.get("front_right_motor");
+        backRight = hardwareMap.dcMotor.get("back_right_motor");
+        if(opModeIsActive()) {
+            double wheelsPowerFactor = 0.6;
+            while(opModeIsActive()) {
+                double output_x = Math.pow(gamepad1.left_stick_x, 3);
+                double output_y = Math.pow(gamepad1.left_stick_y, 3);
+                double output_xRight = Math.pow(gamepad1.right_stick_x, 3);
+                double drive = -output_y * wheelsPowerFactor;//vertical movement = the left stick on controller one(moving on the y-axis)
+                double strafe = -(output_x * wheelsPowerFactor);//Strafing = the left stick on controller 1(moving on the x-axis)
+                double rotate = output_xRight * wheelsPowerFactor;
+                frontLeft.setPower(drive - (strafe - rotate));
+                backLeft.setPower(drive + strafe + rotate);
+                frontRight.setPower(-(drive + (strafe - rotate)));
+                backRight.setPower(-(drive - (strafe + rotate)));
+            }
+        }
     }
 }
